b1dfaca7389b5cc286a71498d80aa96217c4f904
Edgar Joel <esanchez1169@uta.edu.ec>
2025-03-24 10:49:40 -0500

    bad actualizada

--- pagoplux_db.sql
+++ pagoplux_db.sql
@@ -5,14 +5,14 @@
  Source Server Type    : PostgreSQL
  Source Server Version : 150007 (150007)
  Source Host           : localhost:5432
- Source Catalog        : postgres
+ Source Catalog        : pagoplux_db
  Source Schema         : pagoplux_db
 
  Target Server Type    : PostgreSQL
  Target Server Version : 150007 (150007)
  File Encoding         : 65001
 
- Date: 19/03/2025 11:54:30
+ Date: 24/03/2025 10:22:48
 */
 
 
@@ -47,13 +47,32 @@ CREATE TABLE "pagoplux_db"."papx_pagos" (
   "user_id" int8,
   "monto" numeric(10,2),
   "descripcion" varchar(255) COLLATE "pg_catalog"."default",
-  "fecha_pago" timestamptz(6)
+  "fecha_pago" timestamptz(6),
+  "monto_cero" numeric(10,2),
+  "monto_12" numeric(10,2),
+  "whatsapp" varchar(20) COLLATE "pg_catalog"."default",
+  "ci" varchar(15) COLLATE "pg_catalog"."default",
+  "direccion" varchar(255) COLLATE "pg_catalog"."default",
+  "nombre_pago" varchar(255) COLLATE "pg_catalog"."default",
+  "email_pago" varchar(255) COLLATE "pg_catalog"."default",
+  "telefono" varchar(20) COLLATE "pg_catalog"."default",
+  "parent_id" varchar(255) COLLATE "pg_catalog"."default"
 )
 ;
 
 -- ----------------------------
 -- Records of papx_pagos
 -- ----------------------------
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (1, 1, 150.75, 'Pago de prueba por Postman', '2025-03-19 12:31:36.688236-05', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (2, 1, 150.75, 'Pago de prueba por Postman', '2025-03-19 13:34:39.665136-05', 2.00, 3.00, '+593993778542', '1710010002', 'Vencedores y Acacias', 'Nombre Cliente', 'email_cliente@correo.com', '0999777888', NULL);
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (3, 1, 150.75, 'Pago de prueba por Postman', '2025-03-19 13:46:23.915917-05', 2.00, 3.00, '+593989353272', '1710010002', 'Vencedores y Acacias', 'Nombre Cliente', 'email_cliente@correo.com', '0999777888', NULL);
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (4, 1, 150.75, 'Pago de prueba por Postman', '2025-03-19 14:17:51.300583-05', 2.00, 3.00, '+593989353272', '1710010002', 'Vencedores y Acacias', 'Nombre Cliente', 'email_cliente@correo.com', '0999777888', 'd53ed1cf-18db-42db-9d40-f6087966d8a0');
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (5, 1, 150.75, 'Pago de prueba por Postman', '2025-03-23 12:03:12.805473-05', 2.00, 3.00, '+593989353272', '1710010002', 'Vencedores y Acacias', 'Nombre Cliente', 'email_cliente@correo.com', '0999777888', NULL);
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (6, 1, 150.75, 'Pago de prueba por Postman', '2025-03-23 14:13:05.573882-05', 2.00, 3.00, '+593989353272', '1710010002', 'Vencedores y Acacias', 'Nombre Cliente', 'email_cliente@correo.com', '0999777888', NULL);
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (7, 1, 150.75, 'Pago de prueba por Postman', '2025-03-23 14:15:17.242177-05', 2.00, 3.00, '+593989353272', '1710010002', 'Vencedores y Acacias', 'Nombre Cliente', 'email_cliente@correo.com', '0999777888', NULL);
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (8, 1, 1500.00, 'Compra de iPhone 15 Pro Max', '2025-03-23 14:46:51.696775-05', 5.00, 1500.00, '+593989353272', '1850201169', 'Ambato', 'Nombre Tarjetahabiente', 'correocliente@gmail.com', '0989353272', 'dc336635-491a-4643-a33c-215ed6b660a7');
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (9, 1, 1500.00, 'Compra de iPhone 15 Pro Max', '2025-03-23 15:04:43.40518-05', 5.00, 1500.00, '+593989353272', '1850201169', 'Ambato', 'Nombre Tarjetahabiente', 'correocliente@gmail.com', '0989353272', 'c6ce0982-382e-464b-b867-37d3dd246b80');
+INSERT INTO "pagoplux_db"."papx_pagos" VALUES (10, 1, 1500.00, 'Compra de iPhone 15 Pro Max', '2025-03-23 16:03:36.824935-05', 5.00, 1500.00, '+593989353272', '1850201169', 'Ambato', 'Nombre Tarjetahabiente', 'correocliente@gmail.com', '0989353272', '493ed351-b4a7-4166-af1d-8f225e115415');
 
 -- ----------------------------
 -- Table structure for papx_users
@@ -70,12 +89,12 @@ CREATE TABLE "pagoplux_db"."papx_users" (
 -- ----------------------------
 -- Records of papx_users
 -- ----------------------------
-INSERT INTO "pagoplux_db"."papx_users" VALUES (1, 'esanchez', 'MTIzNGpvZWw=', '2025-03-19 11:15:10-05');
+INSERT INTO "pagoplux_db"."papx_users" VALUES (1, 'esanchez', '$2a$10$TQwkrpBPK7TDJw6yJJAnw.6V8.0A78ru5btSrBvtIhuADYEQ2M8ea', '2025-03-19 11:15:10-05');
 
 -- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
-SELECT setval('"pagoplux_db"."papx_pagos_seq"', 1, false);
+SELECT setval('"pagoplux_db"."papx_pagos_seq"', 10, true);
 
 -- ----------------------------
 -- Alter sequences owned by
